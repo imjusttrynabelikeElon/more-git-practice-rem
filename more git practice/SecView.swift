45d058c2ac9e9237628b4e10fcbd7544fed8aab2
karon bell <karonbell5218@gmail.com>
2022-06-02 17:20:25 -0400

    Create Sec View

---
+++ more git practice/SecView.swift
@@ -0,0 +1,20 @@
+//
+//  SecView.swift
+//  more git practice
+//
+//  Created by Karon Bell on 6/2/22.
+//
+
+import SwiftUI
+
+struct SecView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecView()
+    }
+}
