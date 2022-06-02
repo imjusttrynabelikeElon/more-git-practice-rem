ac3c859ebd40779f7cb3db3d487245630d7d8bbb
karon bell <karonbell5218@gmail.com>
2022-06-02 17:16:06 -0400

    changed test content

--- more git practice/ContentView.swift
+++ more git practice/ContentView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        
+        
+        // hey yall
+        Text("This is a Test!")
             .padding()
     }
 }
