8af68d8d553cfefce2fd8abcb3691cc23aa0df97
karon bell <karonbell5218@gmail.com>
2022-06-02 17:11:38 -0400

    Initial Commit

---
+++ more git practice/more_git_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  more_git_practiceApp.swift
+//  more git practice
+//
+//  Created by Karon Bell on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct more_git_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
